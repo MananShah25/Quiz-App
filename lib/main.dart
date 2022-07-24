529eb51fd017a91b6dd37515403c2adef587a857
MananShah25 <mananshah2502@gmail.com>
2022-07-24 16:19:16 +0530

    Commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
@@ -38,9 +37,10 @@ class _QuizPageState extends State<QuizPage> {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
       if (quizBrain.isFinished()) {
-        //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+        if (userPickedAnswer == correctAnswer) {
+          rightAns++;
+        }
         Alert(
           context: context,
           type: AlertType.error,
@@ -58,13 +58,10 @@ class _QuizPageState extends State<QuizPage> {
           ],
         ).show();
         //HINT! Step 4 Part B is in the quiz_brain.dart
-        //TODO: Step 4 Part C - reset the questionNumber,
         quizBrain.reset();
-        //TODO: Step 4 Part D - empty out the scoreKeeper.
         scoreKeeper = [];
         rightAns = 0;
       } else {
-        //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
         if (userPickedAnswer == correctAnswer) {
           scoreKeeper.add(Icon(
             Icons.check,
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -37,6 +37,10 @@ class QuizBrain {
     }
   }
 
+  // int getQuestionNo(){
+  //   return _questionNumber;
+  // }
+
   String getQuestionText() {
     return _questionBank[_questionNumber].questionText;
   }
@@ -45,7 +49,6 @@ class QuizBrain {
     return _questionBank[_questionNumber].questionAnswer;
   }
 
-  //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
   bool isFinished() {
     if (_questionNumber == _questionBank.length - 1) {
       return true;
@@ -54,12 +57,10 @@ class QuizBrain {
     }
   }
 
-  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
   void printResult() {
     print(isFinished());
   }
 
-  //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
   void reset() {
     if (isFinished()) {
       _questionNumber = 0;
